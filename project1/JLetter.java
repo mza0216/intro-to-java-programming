c93bcbff89570a01ee6f8bc1bc5cbfe00ae3cc72
mza0216 <148734634+mza0216@users.noreply.github.com>
2024-12-12 11:39:06 -0800

    Add files via upload

---
+++ project1/JLetter.java
@@ -0,0 +1,28 @@
+/** 
+ * This simple program prints out the letter J using
+ * a combination of the letters in JAVA for each line.
+ *
+ * M1: Project 1
+ * @author Marc Atienza CPSC-1210-AO1
+ * @version 8/20/2021
+ */
+ 
+public class JLetter {
+    /**
+     * Prints out letter J.    
+     * @param args Command line arguments – not used.
+     */
+   public static void main(String[]args) {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA"); 
+   }
+}
+
